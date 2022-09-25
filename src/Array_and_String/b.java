62c2a561b894e384c7c5b8c111ff13f5756c3ddd
vanshpatel <vanshpatel1299@gmail.com>
2022-09-25 23:41:59 +0530

    DSA init

---
+++ src/Array_and_String/b.java
@@ -0,0 +1,15 @@
+package Array_and_String;
+
+public class b extends vansh {
+    static int a;
+    public b() {
+
+        this.a = 5;
+        super();
+    }
+
+    public static void main(String[] args) {
+        b obj = new b();
+        System.out.println(b.a);
+    }
+}
---
+++ src/Array_and_String/vansh.java
@@ -0,0 +1,10 @@
+package Array_and_String;
+
+public class vansh {
+    public static void abc(){
+
+    }
+    public vansh(){
+        System.out.println("call to parent");
+    }
+}
